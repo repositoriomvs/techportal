72ad81c95384a43a1fb5889b852164710a546aec
repositoriomvs <repositorio.mvs@gmail.com>
2026-03-13 15:10:24 -0300

    Update hardwaremodelo.php

--- app/Models/hardwaremodelo.php
+++ app/Models/hardwaremodelo.php
@@ -1,23 +1,18 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class HardwareModelo extends Model
 {
-    protected $fillable = [
-        'hardware_marca_id', 'nombre',
-        'numero_parte', 'descripcion'
-    ];
+    protected $fillable = ['hardware_marca_id', 'nombre', 'numero_parte', 'descripcion'];
 
-  public function marca()
-{
-    return $this->belongsTo(HardwareMarca::class, 'hardware_marca_id', 'id');
-}
+    public function marca()
+    {
+        return $this->belongsTo(\App\Models\HardwareMarca::class, 'hardware_marca_id', 'id');
+    }
 
     public function recursos()
     {
-        return $this->hasMany(HardwareRecurso::class);
+        return $this->hasMany(\App\Models\HardwareRecurso::class);
     }
 }
