e08b646130e1be19ddbe80ee7b5de509507be33f
repositoriomvs <repositorio.mvs@gmail.com>
2026-03-13 15:10:13 -0300

    Update hardwaremarca.php

--- app/Models/hardwaremarca.php
+++ app/Models/hardwaremarca.php
@@ -1,7 +1,5 @@
 <?php
-
 namespace App\Models;
-
 use Illuminate\Database\Eloquent\Model;
 
 class HardwareMarca extends Model
@@ -9,12 +7,12 @@ class HardwareMarca extends Model
     protected $fillable = ['hardware_tipo_id', 'nombre', 'icono'];
 
     public function tipo()
-{
-    return $this->belongsTo(HardwareTipo::class, 'hardware_tipo_id', 'id');
-}
+    {
+        return $this->belongsTo(\App\Models\HardwareTipo::class, 'hardware_tipo_id', 'id');
+    }
 
     public function modelos()
     {
-        return $this->hasMany(HardwareModelo::class);
+        return $this->hasMany(\App\Models\HardwareModelo::class);
     }
 }
